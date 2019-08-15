1abd6f534f10c1ecdb091dbaedfdd7c0e3ee23cb
Hedgy <hedgy134117@hotmail.com>
2019-08-15 12:41:52 -0400

    Cat Select Screen
    Added and finished the cat select screen. Properly linked up with the other scenes

---
+++ Triple Cat Deluxe/Assets/UIPlayerSelectingLabel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIPlayerSelectingLabel : MonoBehaviour {
+
+    public UICatSelect catManager;
+
+    private void Start()
+    {
+        catManager = GameObject.Find("CatManager").GetComponent<UICatSelect>();
+    }
+
+    private void LateUpdate()
+    {
+        switch(catManager.playerSelecting)
+        {
+            case 0:
+                this.GetComponent<TMP_Text>().text = "";
+                break;
+
+            case 1:
+                this.GetComponent<TMP_Text>().text = "Player One";
+                break;
+
+            case 2:
+                this.GetComponent<TMP_Text>().text = "Player Two";
+                break;
+        }
+    }
+}
