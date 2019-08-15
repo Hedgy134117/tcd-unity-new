6935bb27cd3286a21fa4ba1a6be72c522ff0da60
Hedgy <hedgy134117@hotmail.com>
2019-08-14 20:14:43 -0400

    Initial Commit
    Scoreboard finished and working

--- Triple Cat Deluxe/Assets/ScoreboardManager.cs
+++ Triple Cat Deluxe/Assets/ScoreboardManager.cs
@@ -7,4 +7,20 @@ public class ScoreboardManager : MonoBehaviour {
     public int playerOnePoints;
     public int playerTwoPoints;
 
+    // Put object in dont destroy on load
+    private static ScoreboardManager scoreboardManagerInstane;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        if (scoreboardManagerInstane == null)
+        {
+            scoreboardManagerInstane = this;
+        }
+        else
+        {
+            Object.Destroy(this.gameObject);
+        }
+    }
 }
