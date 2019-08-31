0f0d52a4e004337f742b91a22b0305d59433fab2
Hedgy <hedgy134117@hotmail.com>
2019-08-31 13:33:24 -0400

    Added Countdown timer

---
+++ Triple Cat Deluxe/Assets/CardClock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardClock : MonoBehaviour {
+
+    private CardSetter cardSetter;
+    private float timer;
+
+    private void Start()
+    {
+        cardSetter = GameObject.Find("Card").GetComponent<CardSetter>();
+    }
+
+    private void Update()
+    {
+        timer = cardSetter.timerLength;
+        this.GetComponent<Slider>().value = timer;
+    }
+}
