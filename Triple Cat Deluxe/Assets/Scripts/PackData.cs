a9d7bd0932040dc75a2304a92b01560076732790
Hedgy134117 <hedgy134117@gmail.com>
2020-03-02 20:46:23 -0500

    v1.1

---
+++ Triple Cat Deluxe/Assets/Scripts/PackData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Pack", menuName = "Pack", order = 52)]
+public class PackData : ScriptableObject
+{
+    //public string name;
+    //public Image
+}
